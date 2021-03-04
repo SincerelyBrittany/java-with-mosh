a0fdd8d139d438370deb4f60de97ebb2c0d4d760
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-03 22:52:28 -0500

    adds the quickstart class

--- QuickStart.java
+++ QuickStart.java
@@ -1,15 +1,15 @@
 import java.util.Date;
 import java.awt.*;
 
-// class QuickStart {
-//     public static void main(String[] args) {
-//         byte age = 30;
-//         Date now = new Date();
-//         now.getTime();
-//         System.out.println("Hello, World.");
-//         System.out.println(now);
-//     }
-// }
+class QuickStart {
+    public static void main(String[] args) {
+        byte age = 30;
+        Date now = new Date();
+        now.getTime();
+        System.out.println("Hello, World.");
+        System.out.println(now);
+    }
+}
 
 
 // class QuickStart {
@@ -22,13 +22,13 @@ import java.awt.*;
 //     }
 // }
 
-class QuickStart {
-    public static void main(String[] args) {
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
-        // references the same object in memory
-        //reference types are copied by their references
-    }
-}
+// class QuickStart {
+//     public static void main(String[] args) {
+//         Point point1 = new Point(1, 1);
+//         Point point2 = point1;
+//         point1.x = 2;
+//         System.out.println(point2);
+//         // references the same object in memory
+//         //reference types are copied by their references
+//     }
+// }
