03ce779c2e5d427a4725675f2c98a941071f9cd2
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-05 15:39:30 -0500

    adds if statment notes

---
+++ IfStatements.java
@@ -0,0 +1,13 @@
+public class IfStatements {
+     public static void main(String[] args) {
+         int temp = 32; 
+         if (temp > 30){
+           System.out.println("It is a hot day");
+           System.out.println("Drink Water");
+        } else if(temp > 20 && temp <= 30){
+            System.out.println("beautiful day");
+        }else{
+            System.out.println("cold day");
+        }
+    }
+}
