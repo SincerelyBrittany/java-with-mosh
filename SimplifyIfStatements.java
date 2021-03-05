0a57cadc20cc3a07a9e946898df009d29c70ef3b
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-05 15:45:02 -0500

    adds a way to simplify if statements on one line

---
+++ SimplifyIfStatements.java
@@ -0,0 +1,17 @@
+public class SimplifyIfStatements {
+     public static void main(String[] args) {
+        // int income = 120_000;
+        // boolean hasHighIncome; //must declare outside of if statement 
+        // if(income > 100_000){
+        //     hasHighIncome = true;
+        // }else{
+        //     hasHighIncome = false;
+        // }
+        // System.out.println(hasHighIncome);
+
+
+        int income = 120_000;
+        boolean hasHighIncome = income > 100_000; 
+        System.out.println(hasHighIncome);
+    }
+}
