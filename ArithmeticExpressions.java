7837ca26a90c4cc143f0faf33bfea5b213bebff2
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-04 22:05:29 -0500

    adds arithmetic expression notes

---
+++ ArithmeticExpressions.java
@@ -0,0 +1,24 @@
+public class ArithmeticExpressions {
+      public static void main(String[] args) {
+       // + - * / %
+       int result = 20 + 3;
+       System.out.println(result);
+
+       int divison = 10/3;
+       System.out.println(divison);
+
+       double divison2 = (double)10/(double)3;
+       System.out.println(divison2);
+
+        int x = 1;
+        x++; //increment
+        //x + x +2;
+        //x += 2;
+        int y = x++;
+        int z = ++x;
+       System.out.println(x);
+       System.out.println(y);
+       System.out.println(z);
+
+    }
+}
