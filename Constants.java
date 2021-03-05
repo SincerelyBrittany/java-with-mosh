1031e4e9545658596d101d296f6fe57886f51333
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-04 21:56:02 -0500

    adds constants notes

---
+++ Constants.java
@@ -0,0 +1,10 @@
+public class Constants {
+    public static void main(String[] args) {
+        // There are times when you dont want the value of a variable to change - use constant
+        float pi = 3.14F;
+        pi = 1;
+
+        final float NUM = 3.14F; 
+
+    }
+}
