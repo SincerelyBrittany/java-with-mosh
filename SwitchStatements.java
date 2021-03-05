af48f2ce4e995aa3d1d226a2d8909061f0a87f22
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-05 15:53:52 -0500

    adds notes on java switch statments versus if statment

---
+++ SwitchStatements.java
@@ -0,0 +1,29 @@
+public class SwitchStatements {
+       public static void main(String[] args) {
+        String role = "admin";
+
+        //If statement 
+
+        // if(role == "admin"){
+        //     System.out.println("You are an admin");
+        // }else if(role == "moderator"){
+        //      System.out.println("You are a moderator");
+        // } else{
+        //      System.out.println("You are a guest");
+        // }
+
+
+        //SWITCH STATMENT
+
+        switch (role){
+            case "admin":
+                System.out.println("You are an admin");
+                break;
+            case "moderator":
+                System.out.println("You are a moderator");
+                break;
+            default:
+                System.out.println("You are a guest");
+        }
+    } 
+}
