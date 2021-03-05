2b63279ebfae657d9bac63383f9b66b6ec387a5b
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-05 15:35:26 -0500

    adds comparison operator notes

---
+++ ComparisonOperator.java
@@ -0,0 +1,14 @@
+public class ComparisonOperator {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+
+        // = is for assignment 
+        // == comparison operator
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x > y);
+        // System.out.println(x <= y);
+
+    }
+}
