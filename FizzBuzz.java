f6c6b7e29de44f34f1ddd21b090ff237c3e28ca1
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-07 14:49:06 -0500

    adds fizzbuzz notes

---
+++ FizzBuzz.java
@@ -0,0 +1,11 @@
+public class FizzBuzz {
+     public static void main(String[] args) {
+
+        //if number divisible by 5 === fizz
+        //if divisible by 3 === buzz
+        // if divisible by both == fizzbuzz
+        // else return the number
+
+        System.out.println("Hello, World.");
+    }
+}
