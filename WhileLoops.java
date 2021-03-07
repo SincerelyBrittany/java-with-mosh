89cc78dfcb4f39ca637ea9605973da1c98cbedce
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-07 15:12:00 -0500

    adds while loops notes

---
+++ WhileLoops.java
@@ -0,0 +1,10 @@
+public class WhileLoops {
+        public static void main(String[] args) {
+        int i = 5;
+        while (i > 0){
+            System.out.println("Hello, World.");
+            i--;
+        }
+        
+    }
+}
