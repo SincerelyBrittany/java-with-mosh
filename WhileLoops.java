38b60b0091c12188e576c1c446367cfe77c30718
SincerelyBrittany <sincerelybr@gmail.com>
2021-03-07 15:17:09 -0500

    adds a while loop example that only stops when user inputs quite

--- WhileLoops.java
+++ WhileLoops.java
@@ -1,9 +1,20 @@
+ import java.util.Scanner;
+
 public class WhileLoops {
         public static void main(String[] args) {
         int i = 5;
         while (i > 0){
             System.out.println("Hello, World.");
             i--;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while(!input.equals("quit")){
+            System.out.print("Input:  ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
         }
         
     }
